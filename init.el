5181101e6dd0638740af07b7a15959af10acaff1
Andrei Benea <andrei.benea@1and1.ro>
2010-12-22 16:23:58 +0200

    C/C++ indentation style.

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(c-default-style (quote ((c-mode . "stroustrup") (c++-mode . "stroustrup") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
  '(emacsw32-eol-check-new-files t)
  '(emacsw32-eol-file-name-lf-list (quote (".*")))
  '(emacsw32-max-frames t)
