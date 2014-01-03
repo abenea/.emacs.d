b34f9961f0f0d4ddb0ef776384820732f00768a7
Andrei Benea <abenea@gmail.com>
2014-01-03 13:53:32 +0200

    clj-refactor

--- my-clojure.el
+++ my-clojure.el
@@ -2,13 +2,15 @@
                       clojure-test-mode
                       cider
                       paredit
-                      paredit-menu))
+                      paredit-menu
+                      clj-refactor))
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
 (require 'paredit-menu)
 (require 'slamhound)
+(require 'clj-refactor)
 (setq nrepl-hide-special-buffers t)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
