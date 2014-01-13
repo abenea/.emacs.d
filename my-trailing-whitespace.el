7ea7402530cbb7b3977f5493222c477f946f449e
Andrei Benea <abenea@gmail.com>
2014-01-13 11:10:59 +0200

    Change whitespace policy

--- my-trailing-whitespace.el
+++ my-trailing-whitespace.el
@@ -1,12 +1,12 @@
 ;; Show trailing whitespace and delete-trailing-whitespace on save
 (defun trailing-whitespace-is-evil ()
   (setq show-trailing-whitespace t)
-  ;;(add-hook 'before-save-hook 'delete-trailing-whitespace nil t)
-  )
+  (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))
 
 (add-hook 'c++-mode-hook 'trailing-whitespace-is-evil)
 (add-hook 'python-mode-hook 'trailing-whitespace-is-evil)
 (add-hook 'cperl-mode-hook 'trailing-whitespace-is-evil)
+(add-hook 'clojure-mode-hook 'trailing-whitespace-is-evil)
 
 (defun auto-delete-trailing-whitespace ()
   (interactive)
