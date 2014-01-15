267c5a2215dde7655fa86b705877492194cb9a7b
Andrei Benea <abenea@gmail.com>
2014-01-16 00:52:28 +0200

    Midje

--- my-clojure.el
+++ my-clojure.el
@@ -22,16 +22,29 @@
           (lambda ()
             (nrepl-send-string-sync "(System/setOut (java.io.PrintStream. (org.apache.commons.io.output.WriterOutputStream. *out*)))")))
 
+(defun buffer-contains (fragment)
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (search-forward fragment nil t))))
 
 (defun clojure-reload-and-test ()
   (interactive)
-  (save-excursion
-    (if (clojure-test-mode)
-        (cider-load-current-buffer)
+  (save-window-excursion
+    (save-excursion
+      (when (or (buffer-contains "clojure.test") (buffer-contains "midje.sweet"))
         (clojure-jump-between-tests-and-code))
-    (clojure-test-run-tests)))
+      (cider-load-current-buffer)
+      (clojure-jump-between-tests-and-code)
+      (cider-load-current-buffer)
+      (when (buffer-contains "clojure.test")
+        (clojure-test-run-tests)))))
 
 (add-hook 'clojure-mode-hook
           (lambda ()
+            (local-set-key "" 'cider-jack-in)
             (local-set-key [f12] 'clojure-jump-between-tests-and-code)
-            (local-set-key [C-f12] 'clojure-reload-and-test)))
+            (local-set-key [C-f12] 'clojure-reload-and-test)
+            (define-clojure-indent
+              (facts 1)
+              (fact 1))))
