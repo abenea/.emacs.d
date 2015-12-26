547c1b4abb008308c03e23847df6b139633191fb
Andrei Benea <abenea@gmail.com>
2015-12-26 13:46:26 +0200

    use-package for javascript

--- my/my-javascript.el
+++ my/my-javascript.el
@@ -1,16 +1,14 @@
-(dolist (p '(js3-mode
-             tern
-             flycheck
-             company
-             company-tern))
-  (when (not (package-installed-p p))
-    (package-install p)))
+(use-package flycheck)
+(use-package company)
+(use-package tern)
+(use-package company-tern
+  :config
+  (add-to-list 'company-backends 'company-tern))
 
-(require 'company)
-(add-to-list 'company-backends 'company-tern)
-
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-(add-hook 'js3-mode-hook (lambda ()
-                           (tern-mode t)
-                           (company-mode t)
-                           (flycheck-mode t)))
+(use-package js3-mode
+  :mode "\\.js$"
+  :config
+  (add-hook 'js3-mode-hook (lambda ()
+                             (company-mode t)
+                             (flycheck-mode t)
+                             (tern-mode t))))
