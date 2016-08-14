785a4815d934e78892c1105440575d5078a42544
Andrei Benea <abenea@gmail.com>
2016-08-14 22:57:55 +0300

    Update melpa url

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa-stable.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 (when (not (package-installed-p 'use-package))
